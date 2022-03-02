aa5bb54b249fc785bb043ad959b8c39c8ddea011
Amar Preet Singh <amarpsp10@gmail.com>
2022-03-02 09:09:40 +0530

    firebase Env Variables added

--- firebase/config.js
+++ firebase/config.js
@@ -1,10 +1,10 @@
+import Constants from "expo-constants"
+
 export const config = {
-    apiKey: 'api-key',
-    authDomain: 'project-id.firebaseapp.com',
-    databaseURL: 'https://project-id.firebaseio.com',
-    projectId: 'project-id',
-    storageBucket: 'project-id.appspot.com',
-    messagingSenderId: 'sender-id',
-    appId: 'app-id',
-    measurementId: 'G-measurement-id',
+    apiKey: Constants.manifest.extra.apiKey,
+    authDomain: Constants.manifest.extra.authDomain,
+    projectId: Constants.manifest.extra.projectId,
+    storageBucket: Constants.manifest.extra.storageBucket,
+    messagingSenderId: Constants.manifest.extra.messagingSenderId,
+    appId: Constants.manifest.extra.appId
 }
